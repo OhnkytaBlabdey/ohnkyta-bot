a1b35db2ce6a15a678d587aa17f8f05967cc9728
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 21:25:32 +0800

    ..

--- routes/index.js
+++ routes/index.js
@@ -23,27 +23,31 @@ router.post('/', function (req, res) {
 	// log.info({ body: req.body });
 	log.info({ sender: req.body.sender });
 	log.info({ user_id: req.body.sender.user_id });
+	log.info({ nickname: req.body.sender.nickname });
+	log.info({ message_type: req.body.message_type });
+
 	// log.info('type ', typeof req.body.sender);
-	res.status(200).send({
-		reply: 'hi'
-	});
-	return;
-	// if (
-	// 	req.body.message_type === 'group' &&
-	// 	req.body.sender &&
-	// 	req.body.sender.user_id != 1345832339
-	// ) {
-	// 	res.json({
-	// 		reply: req.body.sebder.nickname + '说：' + req.body.message,
-	// 		auto_escape: false,
-	// 		at_sender: false
-	// 	});
-	// } else {
-	// 	res.json({
-	// 		reply: '其他'
-	// 	});
-	// }
-	// res.status(200);
+	// res.status(200).send({
+	// 	reply: 'hi'
+	// });
+	// return;
+
+	if (
+		req.body.message_type === 'group' &&
+		req.body.sender != null &&
+		req.body.sender.user_id != 1345832339
+	) {
+		res.status(200).json({
+			reply: req.body.sebder.nickname + '说：' + req.body.message,
+			auto_escape: false,
+			at_sender: false
+		});
+	} else {
+		res.statue(200).json({
+			reply: '其他',
+			at_sender: false
+		});
+	}
 	// next();
 });
 
