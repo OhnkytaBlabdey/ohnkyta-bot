2a9168e2ed4457f2412ae0691a9e891295c69a60
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 20:30:37 +0800

    。

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,15 @@ const log = bunyan.createLogger({
 	]
 });
 router.post('/', function (req, res, next) {
+	log.info(req);
 	log.info(req.body);
+	log.info(req.body.sender);
 	const message_type = req.body.message_type;
-	// log.info(req);
-	if (message_type === 'group' && req.body.sender.user_id != 1345832339) {
+	if (
+		message_type === 'group' &&
+		req.body.sender &&
+		req.body.sender.user_id != 1345832339
+	) {
 		res.json({
 			reply: req.body.sebder.card + '说：' + req.body.message,
 			auto_escape: false,
