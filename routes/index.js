f595f0ae816261a84f7b21264dfa46c83628a59f
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 19:41:52 +0800

    add logger

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({
+	name: 'hook',
+	time: new Date().toString(),
+	streams: [
+		{
+			level: 'info',
+			stream: process.stdout // log INFO and above to stdout
+		},
+		{
+			level: 'info',
+			type: 'rotating-file',
+			path: './log/info/infos.log',
+			period: '6h', // daily rotation : '1d'
+			count: 64 // keep copies
+		}
+	]
+});
 router.post('/', function (req, res, next) {
 	const message_type = req.data.message_type;
+	log.info(req.data);
 	if (message_type === 'group' && req.data.sender.user_id != 1345832339) {
-		res.send({
+		res.json({
 			reply: req.data.sebder.card + '说：' + req.data.message,
 			auto_escape: false,
 			at_sender: false
