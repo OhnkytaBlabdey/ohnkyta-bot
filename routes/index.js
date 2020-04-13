cfac8d0ad7b9964ff6e0605baaf74c644d62473a
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 21:21:13 +0800

    ...

--- routes/index.js
+++ routes/index.js
@@ -18,27 +18,32 @@ const log = bunyan.createLogger({
 		}
 	]
 });
+
 router.post('/', function (req, res) {
 	// log.info({ body: req.body });
 	log.info({ sender: req.body.sender });
-	log.info({ sender: req.body.sender.user_id });
-	log.info('type ', typeof req.body.sender);
-	if (
-		req.body.message_type === 'group' &&
-		req.body.sender &&
-		req.body.sender.user_id != 1345832339
-	) {
-		res.json({
-			reply: req.body.sebder.nickname + '说：' + req.body.message,
-			auto_escape: false,
-			at_sender: false
-		});
-	} else {
-		res.json({
-			reply: '其他'
-		});
-	}
-	res.status(200);
+	log.info({ user_id: req.body.sender.user_id });
+	// log.info('type ', typeof req.body.sender);
+	res.status(200).send({
+		reply: 'hi'
+	});
+	return;
+	// if (
+	// 	req.body.message_type === 'group' &&
+	// 	req.body.sender &&
+	// 	req.body.sender.user_id != 1345832339
+	// ) {
+	// 	res.json({
+	// 		reply: req.body.sebder.nickname + '说：' + req.body.message,
+	// 		auto_escape: false,
+	// 		at_sender: false
+	// 	});
+	// } else {
+	// 	res.json({
+	// 		reply: '其他'
+	// 	});
+	// }
+	// res.status(200);
 	// next();
 });
 
