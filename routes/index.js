7a34fcaf7cde8b33c803cda61cf107025e7218b3
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 21:08:48 +0800

    .......

--- routes/index.js
+++ routes/index.js
@@ -21,10 +21,10 @@ const log = bunyan.createLogger({
 router.post('/', function (req, res) {
 	log.info({ query: req.query });
 	log.info({ body: req.body });
-	log.info({ sender: req.body['sender'] });
-	log.info({ sender: req.body['sender']['nickname'] });
+	log.info({ sender: req.body.sender });
+	log.info({ sender: req.body.sender.user_id });
 	const body = req.body;
-	log.info('type ', typeof(body.sender));
+	log.info('type ', typeof body.sender);
 	if (
 		body['message_type'] === 'group' &&
 		body['sender'] &&
