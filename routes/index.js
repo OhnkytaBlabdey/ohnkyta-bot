6cafd434b3ab82230f0e7a63027993d8d025ac22
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 19:31:38 +0800

    remove default content add eslintrc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	const message_type = req.data.message_type;
+	if (message_type === 'group' && req.data.sender.user_id != 1345832339) {
+		res.send({
+			reply: req.data.sebder.card + '说：' + req.data.message,
+			auto_escape: false,
+			at_sender: false
+		});
+	}
+	next();
 });
 
 module.exports = router;
