4c147b0a2dc006990f9b40839b5a48ef9014679c
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 21:43:14 +0800

    去除多余调试信息

--- routes/index.js
+++ routes/index.js
@@ -7,52 +7,37 @@ const log = bunyan.createLogger({
 	streams: [
 		{
 			level: 'info',
-			stream: process.stdout // log INFO and above to stdout
+			stream: process.stdout
 		},
 		{
 			level: 'info',
 			type: 'rotating-file',
 			path: './log/info/infos.log',
-			period: '6h', // daily rotation : '1d'
-			count: 64 // keep copies
+			period: '6h',
+			count: 64
 		}
 	]
 });
 
 router.post('/', function (req, res) {
-	// log.info({ body: req.body });
 	log.info({ message: req.body.message });
 	log.info({ sender: req.body.sender });
-	log.info({ user_id: req.body.sender.user_id });
-	log.info({ nickname: req.body.sender.nickname });
 	log.info({ message_type: req.body.message_type });
 
-	// log.info('type ', typeof req.body.sender);
-	// res.status(200).send({
-	// 	reply: 'hi'
-	// });
-	// return;
 	try {
-		if (
-			req.body.message_type == 'group' &&
-			req.body.sender.user_id != 1345832339
-		) {
+		if (req.body.message_type === 'group') {
 			res.status(200).send({
-				reply: req.body.sender.nickname + '说：' + req.body.message,
+				reply:
+					(req.body.sender.card || req.body.sender.nickname) +
+					'说：' +
+					req.body.message,
 				auto_escape: false,
-				at_sender: false
-			});
-		} else {
-			res.statue(200).send({
-				reply: '其他',
 				at_sender: false
 			});
 		}
 	} catch (error) {
 		log.warn(error);
-		// log.trace(error);
 	}
-	// next();
 });
 
 module.exports = router;
