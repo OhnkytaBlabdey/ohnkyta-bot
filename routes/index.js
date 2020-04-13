842e30729a1ac6ef6ebe2d7b1f402c81e8030252
OhnkytaBlabdey <1263189143@qq.com>
2020-04-13 23:17:04 +0800

    运势功能 没有记忆

--- routes/index.js
+++ routes/index.js
@@ -25,15 +25,26 @@ router.post('/', function (req, res) {
 	log.info({ message_type: req.body.message_type });
 
 	try {
-		if (req.body.message_type === 'group' && req.body.message.length > 18) {
-			res.status(200).send({
-				reply:
-					(req.body.sender.card || req.body.sender.nickname) +
-					'说：' +
-					req.body.message,
-				auto_escape: false,
-				at_sender: false
-			});
+		if (req.body.message_type === 'group') {
+			if (req.body.message.length > 128) {
+				res.status(200).send({
+					reply:
+						(req.body.sender.card || req.body.sender.nickname) +
+						'说：' +
+						req.body.message,
+					auto_escape: false,
+					at_sender: false
+				});
+			} else if (/今日运势/.match(req.body.message)) {
+				res.status(200).send({
+					auto_escape: false,
+					at_sender: false,
+					reply:
+						(req.body.sender.card || req.body.sender.nickname) +
+						'今天的运势是' +
+						Math.floor(Math.random() * 100)
+				});
+			}
 		} else {
 			res.status(200);
 		}
