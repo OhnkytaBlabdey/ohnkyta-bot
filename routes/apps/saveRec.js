53b6a59b84e88466e8f68cf7ce4ef7494bd740ce
OhnkytaBlabdey <1263189143@qq.com>
2020-04-14 22:00:20 +0800

    .....

--- routes/apps/saveRec.js
+++ routes/apps/saveRec.js
@@ -7,9 +7,10 @@ const crypto = require('crypto');
 
 const handle = (timestamp, msg, groupid, userid) => {
 	if (msg.length > 256) msg = '长消息';
-	const sha256 = crypto.createHash('sha256');
+	let sha256 = crypto.createHash('sha256');
 	sha256.update(groupid + '');
 	const group = sha256.digest('hex');
+	sha256 = crypto.createHash('sha256');
 	sha256.update(userid + '');
 	const user = sha256.digest('hex');
 	MsgRec.create({
