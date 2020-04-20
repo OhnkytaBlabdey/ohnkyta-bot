8d6f4ed5e69425780408ca784628df2467cf8b8f
OhnkytaBlabdey <1263189143@qq.com>
2020-04-21 00:35:28 +0800

    qaq

--- routes/apps/jielong.js
+++ routes/apps/jielong.js
@@ -12,100 +12,110 @@ let last = {};
  * @returns {string} desc
  */
 const handle = async (user_id, card_name) => {
-	log.info('处理接龙');
-	if (!user_id || !card_name) {
-		return {
-			status: 'fail',
-			desc: '不完整'
-		};
-	}
-	Card.findOne({
-		where: {
-			name: card_name
-		},
-		attributes: ['head', 'tail']
-	})
-		.then((card) => {
-			// 验证输入存在性
-			if (card) {
-				log.info('found');
-				if (last[user_id] && last[user_id] != card.head) {
-					// 验证输入正确性
-					// try {
-					// 	async () => {
-					// 		const res = await Card.count({
-					// 			where: {
-					// 				head: last[user_id].tail
-					// 			}
-					// 		});
-					// 	};
-					// } catch (error) {
-					// 	log.warn(error);
-					// 	return '内部错误3';
-					// }
-					log.info('failed');
-					return {
-						status: 'fail',
-						desc: '你输了，接龙结束'
-					};
-				}
-				// 回答
-				Card.findAll({
-					where: {
-						head: card.tail
-					},
-					attributes: ['name', 'tail']
-				})
-					.then((cards) => {
-						log.info('reply');
-						if (cards && cards.length >= 1) {
-							let c = cards[Math.floor(Math.random() * cards.length)];
-							last[user_id] = c.tail;
-							return {
-								status: 'ok',
-								desc: c.name
-							};
-						} else {
-							log.info('win');
-							if (last[user_id]) {
-								return {
-									status: 'win',
-									desc: '你赢了'
-								};
-							} else {
-								return {
-									status: 'fail',
-									desc: '???'
-								};
-							}
-						}
-					})
-					.catch((err) => {
-						if (err) {
-							log.warn(err);
-							return {
-								status: 'error',
-								desc: '内部错误2'
-							};
-						}
-					});
-			} else {
-				log.info('not exists');
-				return {
-					status: 'fail',
-					desc: '你输了，这个卡名不存在'
-				};
-			}
+	return new Promise((x) => {
+		log.info('处理接龙');
+		if (!user_id || !card_name) {
+			x({
+				status: 'fail',
+				desc: '不完整'
+			});
+			return;
+		}
+		Card.findOne({
+			where: {
+				name: card_name
+			},
+			attributes: ['head', 'tail']
 		})
-		.catch((err) => {
-			if (err) {
-				log.warn(err);
-				return {
-					status: 'error',
-					desc: '内部错误1'
-				};
-			}
-		});
+			.then((card) => {
+				// 验证输入存在性
+				if (card) {
+					log.info('found');
+					if (last[user_id] && last[user_id] != card.head) {
+						// 验证输入正确性
+						// try {
+						// 	async () => {
+						// 		const res = await Card.count({
+						// 			where: {
+						// 				head: last[user_id].tail
+						// 			}
+						// 		});
+						// 	};
+						// } catch (error) {
+						// 	log.warn(error);
+						// 	return '内部错误3';
+						// }
+						log.info('failed');
+						x({
+							status: 'fail',
+							desc: '你输了，接龙结束'
+						});
+						return;
+					}
+					// 回答
+					Card.findAll({
+						where: {
+							head: card.tail
+						},
+						attributes: ['name', 'tail']
+					})
+						.then((cards) => {
+							log.info('reply');
+							if (cards && cards.length >= 1) {
+								let c = cards[Math.floor(Math.random() * cards.length)];
+								last[user_id] = c.tail;
+								x({
+									status: 'ok',
+									desc: c.name
+								});
+								return;
+							} else {
+								log.info('win');
+								if (last[user_id]) {
+									x({
+										status: 'win',
+										desc: '你赢了'
+									});
+									return;
+								} else {
+									x({
+										status: 'fail',
+										desc: '???'
+									});
+									return;
+								}
+							}
+						})
+						.catch((err) => {
+							if (err) {
+								log.warn(err);
+								x({
+									status: 'error',
+									desc: '内部错误2'
+								});
+								return;
+							}
+						});
+				} else {
+					log.info('not exists');
+					x({
+						status: 'fail',
+						desc: '你输了，这个卡名不存在'
+					});
+					return;
+				}
+			})
+			.catch((err) => {
+				if (err) {
+					log.warn(err);
+					x({
+						status: 'error',
+						desc: '内部错误1'
+					});
+					return;
+				}
+			});
+	});
 };
 
 module.exports = handle;
--- test.js
+++ test.js
@@ -1,5 +1,16 @@
-const jielong = require('./routes/apps/jielong');
+// const jielong = require('./routes/apps/jielong');
+const jielong = async (id, name) => {
+	return new Promise((x) => {
+		setTimeout(() => {
+			x({
+				status: 'ok'
+			});
+		}, 2000);
+	});
+};
+
 (async () => {
 	const rep = await jielong(123, '龙子');
-	console.log('rep' + rep);
+	console.log('rep : ' + (rep.status));
+	console.log('rep : ' + JSON.stringify(rep));
 })();
