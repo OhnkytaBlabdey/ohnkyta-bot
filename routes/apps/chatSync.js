3d7ebbd57f00daaeb04ecbd78ae76cc18c48f126
OhnkytaBlabdey <1263189143@qq.com>
2020-07-27 20:10:33 +0800

    ..

--- routes/apps/chatSync.js
+++ routes/apps/chatSync.js
@@ -2,12 +2,13 @@
 const log = require('./logger');
 const axios = require('axios');
 const sha256 = require('crypto-js/sha256');
+const config = require('../../config.json');
 const handle = (title, card, msg) => {
 	const url = 'http:/cn-zz-/bgp.sakurafrp.com:10240';
 	const dat = ((title && '【' + title + '】') || '') + card + '：' + msg;
 	const timestamp = new Date().valueOf();
-	const hash = sha256(dat + timestamp + 'wabiwabi');
-	log.info(dat, timestamp, hash);
+	const hash = sha256(dat + timestamp + config['salt']);
+	log.info('同步群聊', dat, timestamp, hash);
 	axios.default.post(url, {
 		msg: dat,
 		time: timestamp,
