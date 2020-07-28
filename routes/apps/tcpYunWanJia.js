1ba1bf61f0ca4766fe8a8f9787e55ff9775d90be
OhnkytaBlabdey <1263189143@qq.com>
2020-07-28 12:20:14 +0800

    ..

--- routes/apps/tcpYunWanJia.js
+++ routes/apps/tcpYunWanJia.js
@@ -10,37 +10,34 @@ const options = {
 	port: 10240
 };
 
-const tcp_client = net.Socket();
-
-// 接收数据
-tcp_client.on('data', function (data) {
-	log.info('received data: %s from server', data.toString());
-	handler.conn = true;
-});
-
-tcp_client.on('end', function () {
-	log.info('data end!');
-});
-
-tcp_client.on('error', function (e) {
-	if (e.errno === 'ETIMEDOUT') return;
-	log.warn('tcp_client error!', e);
-});
 handler.send = (str) => {
-	tcp_client.write(str);
+	handler.tcp_client.write(str);
 };
 handler.checkConn = () => {
 	return handler.conn;
 };
 handler.getConnAndSend = (str) => {
+	handler.tcp_client = net.Socket();
+	// 接收数据
+	handler.tcp_client.on('data', function (data) {
+		log.info('received data: %s from server', data.toString());
+		handler.conn = true;
+	});
+	handler.tcp_client.on('end', function () {
+		log.info('data end!');
+	});
+	handler.tcp_client.on('error', function (e) {
+		if (e.errno === 'ETIMEDOUT') return;
+		log.warn('tcp_client error!', e);
+	});
 	// 连接 tcp server
 	// 会有连接超时错误
-	tcp_client.connect(options, function () {
+	handler.tcp_client.connect(options, function () {
 		log.info('connected to Server');
 	});
 	setTimeout(() => {
 		if (!handler.conn) {
-			tcp_client.end();
+			handler.tcp_client.end();
 			log.info('tcp server not online');
 		} else {
 			handler.send(str);
