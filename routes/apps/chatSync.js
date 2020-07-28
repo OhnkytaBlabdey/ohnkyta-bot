d175592a5100402162385de0b7f18d272cab0e9f
OhnkytaBlabdey <1263189143@qq.com>
2020-07-28 12:27:35 +0800

    ..

--- routes/apps/chatSync.js
+++ routes/apps/chatSync.js
@@ -1,12 +1,14 @@
 'use-strict';
 const log = require('./logger');
-const sha256 = require('crypto-js/sha256');
+const crypto = require('crypto');
 const config = require('../../config.json');
 const tcpHandler = require('./tcpYunWanJia');
 const handle = (title, card, msg) => {
 	const dat = ((title && '【' + title + '】') || '') + card + '：' + msg;
 	const timestamp = new Date().valueOf();
-	const hash = sha256(dat + timestamp + config['salt']).toString();
+	const sha256 = crypto.createHash('sha256');
+	sha256.update(dat + timestamp + config['salt']);
+	const hash = sha256.digest('hex');
 	log.info('同步群聊', dat, timestamp, hash);
 	if (tcpHandler.checkConn()) {
 		tcpHandler.send(
