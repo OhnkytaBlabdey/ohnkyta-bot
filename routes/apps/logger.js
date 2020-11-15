b19fe0814fd20173a91f045d6a6239119501dda4
OhnkytaBlabdey <1263189143@qq.com>
2020-11-15 09:34:29 +0800

    日志框架bunyan咕了7年，不支持改时区，先用着0时区

--- routes/apps/logger.js
+++ routes/apps/logger.js
@@ -2,29 +2,38 @@
 
 const bunyan = require('bunyan');
 const path = require('path');
+function MyStream() {}
+MyStream.prototype.write = function (rec) {
+	rec.level = bunyan.nameFromLevel[rec.level];
+	rec.time = new Date().toLocaleString();
+};
+
 const logger = bunyan.createLogger({
 	name: 'bot',
-	time:(()=>{
-		return new Date().toLocaleString();
-	})(),
+	// stream:new MyStream(),
 	streams: [
 		{
 			level: 'info',
 			stream: process.stdout
+			// time:new Date().toLocaleString()
 		},
 		{
 			level: 'info',
 			type: 'rotating-file',
 			path: path.normalize(__dirname + '/../..') + '/log/info/infos.log',
-			period: '2h',
-			count: 8
+			period: '12h',
+			count: 32
+			// stream:new MyStream(),
+			// time:new Date().toLocaleString()
 		},
 		{
 			level: 'warn',
 			type: 'rotating-file',
 			path: path.normalize(__dirname + '/../..') + '/log/warn/warns.log',
 			period: '24h',
-			count: 8
+			count: 64
+			// stream:new MyStream(),
+			// time:new Date().toLocaleString()
 		}
 	]
 });
--- test.js
+++ test.js
@@ -1,14 +1,6 @@
 // const monitor = require('./routes/apps/liveMonitor');
 const log =require('./routes/apps/logger');
-const axios = require('axios');
+// const axios = require('axios');
 
-// (async ()=>{
-// 	log.info(await monitor.isOnLiveAsync(528210));
-// 	log.info(await monitor.getInfo(15126927));
-// })();
 log.info(process.cwd());
-axios.default.post('http://47.102.140.37:9999', {
-	val:'a',
-	date:'b',
-	code:'7151bc597b9a0cca17c41be011f6be6f357070484c382c60f85f2ae9d2624bb9'
-});
+log.info(process.cwd());
