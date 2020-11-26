d9de5a69b1612e1a0ff6a59ac15af185b1815de9
OhnkytaBlabdey <1263189143@qq.com>
2020-11-26 16:37:34 +0800

    色图 报错信息

--- routes/apps/setu.js
+++ routes/apps/setu.js
@@ -6,7 +6,12 @@ const sendReply = require('./Util').sendReply;
 
 const getSetu = (keyword) => {
 	return new Promise((setu) => {
-		fetch('https://api.lolicon.app/setu/?keyword=' + encodeURI(keyword) + '&apikey=' +config.setu_key)
+		fetch(
+			'https://api.lolicon.app/setu/?keyword=' +
+				encodeURI(keyword) +
+				'&apikey=' +
+				config.setu_key
+		)
 			.then((res) => {
 				try {
 					(async () => {
@@ -16,8 +21,7 @@ const getSetu = (keyword) => {
 						if (data.length == 0) {
 							setu(json);
 							return;
-						}
-						else if (data && data[0] != undefined) {
+						} else if (data && data[0] != undefined) {
 							log.debug('色图获取结果' + data[0]);
 							setu(data[0]);
 							return;
@@ -46,8 +50,16 @@ const handleSetu = async (gid, keyword) => {
 			'[CQ:image,file=' + setu.url + ']' + setu.title + '\n' + setu.author
 		);
 	} else {
-		log.warn('获取色图失败');
-		sendReply(gid, '没有符合条件的色图');
+		log.warn('获取色图失败', setu.code);
+		const errorMsgMap = new Map([
+			[-1, '内部错误，请向 i@loli.best 反馈'],
+			[401, 'APIKEY 不存在或被封禁'],
+			[403, '由于不规范的操作而被拒绝调用'],
+			[404, '找不到符合关键字的色图'],
+			[429, '达到调用额度限制']
+		]);
+		const errorMsg = errorMsgMap.get(setu.code);
+		sendReply(gid, errorMsg);
 	}
 };
 
