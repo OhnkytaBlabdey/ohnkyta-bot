612366464e9e965a4020d28471587ce69f61b919
OhnkytaBlabdey <1263189143@qq.com>
2021-01-04 13:06:48 +0800

    fix - 10

--- routes/apps/videoMonitor.js
+++ routes/apps/videoMonitor.js
@@ -85,8 +85,8 @@ monitor.chk = async (id, group_id) => {
 					group_id,
 					vlist[0]['author'] +
 						'更新了视频\n' +
-						+(`b23.tv/av${new String(lav)}`) + '\n' +
-						+(`https://www.bilibili.com/video/${vlist[0]['bvid']}`) + '\n' +
+						// +(''.join('b23.tv/av',lav)) + '\n' +
+						+('https://www.bilibili.com/video/'+vlist[0]['bvid'] + '\n' +
 						vlist[0]['title'] + '\n' +
 						'[CQ:image,file=https:' +
 						vlist[0]['pic'] +
