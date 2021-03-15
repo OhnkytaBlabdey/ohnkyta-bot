b3d5ec080a8b3960465e422db4ba2b93076219bc
小张 <1263189143@qq.com>
2021-03-15 17:21:01 +0800

    Update bot_sync.sh
    Linux系统写法

--- bot_sync.sh
+++ bot_sync.sh
@@ -3,4 +3,4 @@ git reset --hard origin/master
 git clean -f
 git pull origin master
 npm install
-node_modules\\.bin\\cross-env NODE_ENV=production PORT=9961 TZ='Asia/Shanghai' pm2 restart ohnkyta-bot --update-env
+cross-env NODE_ENV=production PORT=9961 TZ='Asia/Shanghai' pm2 restart ohnkyta-bot --update-env
